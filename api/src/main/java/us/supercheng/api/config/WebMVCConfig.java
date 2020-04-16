c74bdb7f654e78d89400e3e930b249d7bbb9da28
Hongyu Cheng <chenghongyu628@gmail.com>
2020-04-16 00:10:50 -0400

    add static file hosting

--- api/src/main/java/us/supercheng/api/config/WebMVCConfig.java
+++ api/src/main/java/us/supercheng/api/config/WebMVCConfig.java
@@ -4,12 +4,21 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMVCConfig {
+
+public class WebMVCConfig implements WebMvcConfigurer {
 
     @Bean
     public RestTemplate restTemplateInit(RestTemplateBuilder restTemplateBuilder) {
         return restTemplateBuilder.build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("file:/Users/cheng/dev/static-files/foodie-dev/avatars/");
     }
 }
