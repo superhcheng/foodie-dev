6fa6bf1794110ca36cf56a7e608ee9ab9e6fa503
Hongyu Cheng <chenghongyu628@gmail.com>
2020-07-05 22:59:00 -0400

    add RedisOperator Util

--- api/src/main/java/us/supercheng/api/controller/RedisTestController.java
+++ api/src/main/java/us/supercheng/api/controller/RedisTestController.java
@@ -2,23 +2,23 @@ package us.supercheng.api.controller;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 import us.supercheng.utils.APIResponse;
+import us.supercheng.utils.RedisOperator;
 
 @RestController
 @RequestMapping("redis_test")
 public class RedisTestController {
 
     @Autowired
-    private RedisTemplate redisTemplate;
+    private RedisOperator redisOperator;
 
     @GetMapping("get")
     public APIResponse getValue(String key) {
         if (StringUtils.isBlank(key))
             return APIResponse.ok();
 
-        return APIResponse.ok(this.redisTemplate.opsForValue().get(key));
+        return APIResponse.ok(redisOperator.get(key));
     }
 
     @PostMapping("set")
@@ -26,7 +26,7 @@ public class RedisTestController {
         if (StringUtils.isBlank(key) || StringUtils.isBlank(value))
             return APIResponse.errorMsg("Empty key and/or value");
 
-        this.redisTemplate.opsForValue().set(key, value);
+        this.redisOperator.set(key, value);
         return APIResponse.ok();
     }
 
@@ -35,7 +35,7 @@ public class RedisTestController {
         if (StringUtils.isBlank(key))
             return APIResponse.ok();
 
-        if (!this.redisTemplate.delete(key))
+        if (!this.redisOperator.del(key))
             return APIResponse.errorMsg("Could not delete key: " + key);
         return APIResponse.ok();
     }
