0dcca02c0828812c22e3c0495dc9baeaddc9474c
Hongyu Cheng <chenghongyu628@gmail.com>
2020-08-07 22:53:17 -0400

    add Auth Inteceptor

--- api/src/main/java/us/supercheng/api/config/WebMVCConfig.java
+++ api/src/main/java/us/supercheng/api/config/WebMVCConfig.java
@@ -1,17 +1,23 @@
 package us.supercheng.api.config;
 
 import org.apache.tomcat.util.http.LegacyCookieProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import us.supercheng.api.interceptor.AuthInterceptor;
 
 @Configuration
 public class WebMVCConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private AuthInterceptor authInterceptor;
 
     @Bean
     public RestTemplate restTemplateInit(RestTemplateBuilder restTemplateBuilder) {
@@ -29,5 +35,25 @@ public class WebMVCConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:META-INF/resources/")          // Swagger2
                 .addResourceLocations("file:/Users/cheng/dev/static-files/foodie-dev/avatars/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/hello")
+                .addPathPatterns("/shopcart/add")
+                .addPathPatterns("/shopcart/del")
+                .addPathPatterns("/address/list")
+                .addPathPatterns("/address/add")
+                .addPathPatterns("/address/update")
+                .addPathPatterns("/address/setDefalut")
+                .addPathPatterns("/address/delete")
+                .addPathPatterns("/orders/*")
+                .addPathPatterns("/center/*")
+                .addPathPatterns("/userInfo/*")
+                .addPathPatterns("/myorders/*")
+                .addPathPatterns("/mycomments/*")
+                .excludePathPatterns("/myorders/deliver")
+                .excludePathPatterns("/orders/notifyMerchantOrderPaid");
     }
 }
---
+++ api/src/main/java/us/supercheng/api/interceptor/AuthInterceptor.java
@@ -0,0 +1,53 @@
+package us.supercheng.api.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import us.supercheng.service.UsersService;
+import us.supercheng.utils.APIResponse;
+import us.supercheng.utils.JsonUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+@Component
+public class AuthInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private UsersService usersService;
+
+    String USER_ID_KEY = "headerUserId";
+    String USER_TOKEN_KEY = "headerUserToken";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String userId = request.getHeader(USER_ID_KEY),
+               userToken = request.getHeader(USER_TOKEN_KEY);
+
+        if (this.usersService.hasUserSession(userId, userToken)) {
+            return true;
+        }
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter writer = response.getWriter();
+        writer.write(JsonUtils.objectToJson(APIResponse.errorMsg("Please login")));
+        writer.flush();
+        writer.close();
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
